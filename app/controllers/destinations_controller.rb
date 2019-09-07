88a3cf1ab71832e27fff484656e3072232595345
delphine cretegny <delphine.cretegny@free.fr>
2019-09-07 02:05:49 +0200

    card responsiveness

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,5 +1,22 @@
 class DestinationsController < ApplicationController
+  DESTINATIONS = {
+    1 => { destination_name: 'Madrid',
+           transport_price: '2.25$',
+           photo_link: 'https://images.kiwi.com/photos/600x600/madrid_es.jpg',
+           banana_price: '1.50$',
+           water_price: '0.50$' },
+    2 => { destination_name: 'Barcelone',
+           transport_price: '2.25$',
+           photo_link: 'https://images.kiwi.com/photos/600x600/barcelona_es.jpg',
+           banana_price: '1.50$',
+           water_price: '0.50$' },
+    3 => { destination_name: 'Porto',
+           transport_price: '2.25$',
+           photo_link: 'https://images.kiwi.com/photos/600x600/porto_pt.jpg',
+           banana_price: '1.50$',
+           water_price: '0.50$' }
+  }
   def index
-    @destinations = Destination.all
+    @destinations = DESTINATIONS
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+require_relative 'destination'
+
+url = "https://www.numbeo.com/cost-of-living/in/Madrid"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+entries = url.css('.data_wide_table')
+transport_price = entries.css('table')[0].css('tr')[30].css('.priceValue').text
+banana_price = entries.css('table')[0].css('tr')[18].css('.priceValue').text
+water_price = entries.css('table')[0].css('tr')[8].css('.priceValue').text
+
+
+html_doc.search('.priceValue').each do |element|
+  puts element.text.strip
+  puts element.attribute('href').value
+end
